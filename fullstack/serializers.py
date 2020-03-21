283a723e886cff9fcf2ea54a6e7cc534ea93163a
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-21 12:15:09 +0300

    Created JWT token when user logs in

--- fullstack/serializers.py
+++ fullstack/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email','password')
+        
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+        
+    
--- fullstack/views.py
+++ fullstack/views.py
@@ -4,14 +4,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 
 class UserList(APIView):
+    permission_classes = (IsAuthenticated,)
     def get(self, request, format=None):
         all_users = User.objects.all()
         serializers = UserSerializer(all_users,many=True)
         return Response(serializers.data)
+    
     def post(self,request,format=None):
         serializers = UserSerializer(data=request.data)
         if serializers.is_valid():
